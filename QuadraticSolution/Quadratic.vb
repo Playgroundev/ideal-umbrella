35570289192ddb169126caba5a7a7f9f92b3cd5d
mtotodev05 <mtotodev05@gmail.com>
2018-03-06 16:00:42 +0300

    Implemented Functions

--- QuadraticSolution/Quadratic.vb
+++ QuadraticSolution/Quadratic.vb
@@ -14,9 +14,14 @@
         A = Val(TxtA.Text)
         B = Val(TxtB.Text)
         C = Val(TxtB.Text)     
-        Result1 = (-B + (Math.Sqrt(Math.Abs(B ^ 2 - 4 * A * C)) / (2 * A)))
+        Result1 = GetSolution(A, B, C)
         Result2 = (-B - (Math.Sqrt(Math.Abs(B ^ 2 - 4 * A * C)) / (2 * A)))
         TxtResult1.Text = Result1
         TxtResult2.Text = Result2
     End Sub
+
+    Function GetSolution(ByVal InputA As Double, ByVal InputB As Double, ByVal InputC As Double) As Double
+        Return (InputA + (Math.Sqrt(Math.Abs(InputB ^ 2 - 4 * InputA * InputC)) / (2 * InputA)))
+    End Function
+
 End Class
